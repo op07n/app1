e86557c632d125af35569c2bf080aefc868f1299
op07n <39182105+op07n@users.noreply.github.com>
2019-10-17 11:34:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-    [general]\n\
-    email = \"your-email@domain.com\"\n\
-    " > ~/.streamlit/credentials.toml
+
 
 echo "\
-    [server]\n\
-    headless = true\n\
-    enableCORS = false\n\
-    " > ~/.streamlit/config.toml
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+enableCORS = false\n\
+headless = true\n\
+[browser]\n\
+gatherUsageStats = false\n\
+" > ~/.streamlit/config.toml
+
